1d066766b7539032b82acf3c7e4a09c4833cab90
dvora wakshtok <dw@gmail.com>
2025-03-17 17:33:08 +0200

    update register

--- online-course-managment/.angular/src/moduls/course .ts
+++
@@ -1,8 +0,0 @@
-export class Course {
-    constructor(
-        public id: number,
-        public title: string,
-        public description: string,
-        public teacherId: number
-    ) { }
-}
--- online-course-managment/.angular/src/moduls/lesson.ts
+++
@@ -1,9 +0,0 @@
-export class Lesson {
-    lessonId: any;
-    constructor(
-        public id: number,
-        public title: string,
-        public content: string,
-        public courseId: number,
-    ) { }
-}
